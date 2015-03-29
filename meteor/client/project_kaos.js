aa70cd49575a581e87201ea7838d06e2a7be97a5
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2015-03-29 04:33:07 -0700

    Added some packages and changed around file structure

---
+++ meteor/client/project_kaos.js
@@ -0,0 +1,21 @@
+// counter starts at 0
+//Session.setDefault('counter', 0);
+
+Template.body.helpers({
+	friends: function(){
+		return Friends.find({}, {sort:{createdAt: -1}});
+	}
+});
+Template.body.events({
+	"submit .new-friend": function(event){
+		var text = event.target.text.value;
+		Friends.insert({
+			text: text,
+			createdAt: new Date()
+			//realName: text//current time
+		});
+		event.targer.text.value="";
+		return false;
+	}
+
+});
