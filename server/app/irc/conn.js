cc1153f0c14d7151fa7107446794c50ec8d144bf
Joseph Warner <hardolaf@hardolaf.com>
2015-03-28 22:40:40 -0400

    Add start of server code

---
+++ server/app/irc/conn.js
@@ -0,0 +1,16 @@
+
+module.exports = function(server, nickname, channels) {
+	var irc = require('irc');
+	var client = new irc.Client(server, nickname, channels);
+
+	client.addlistener('message', function(from, to, message) {
+		// do something
+	});
+
+	client.addlistener('pm', function(from, message) {
+		// do something
+	});
+
+	return client;
+}
+
