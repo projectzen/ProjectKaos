a545268c7ac46e44f5130ab120d274c52aa220f8
Ximone <Ximone@Ximones-MacBook-Air.local>
2015-03-28 23:34:42 -0500

    Initial meteor app files

---
+++ project_kaos.js
@@ -0,0 +1,41 @@
+Friends = new Mongo.Collection("friends");
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  //Session.setDefault('counter', 0);
+
+  Template.body.helpers({
+    friends: function(){
+      return Friends.find({});
+    }
+  });
+    Template.body.events({
+       "submit .new-friend": function(event){
+             var text = event.target.text.value;
+          Friends.insert({
+             text: text,
+           createdAt: new Date()
+                         //realName: text//current time
+          });
+          event.targer.text.value="";
+          return false;
+        }
+        
+   });
+    
+}
+
+ /* Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+  */
